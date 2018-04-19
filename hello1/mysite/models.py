1f499ac08d88435b65a2625466d091dbf70cd47a
apojuice <490049790@qq.com>
2018-04-19 20:01:11 +0800

    添加主页 详情

--- hello1/mysite/models.py
+++ hello1/mysite/models.py
@@ -11,4 +11,13 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= (timezone.now() - datetime.timedelta(days=1))
+        return (self.pub_date >= (timezone.now() - datetime.timedelta(days=1))) and (self.pub_date <= timezone.now())
+
+class Choice(models.Model):
+    # models.CASCADE 级联删除
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
