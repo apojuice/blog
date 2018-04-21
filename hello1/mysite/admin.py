f043a946c0605a2d4ad2074bdcb8d0b21b990b54
apojuice <490049790@qq.com>
2018-04-21 16:22:00 +0800

    简单的投票

--- hello1/mysite/admin.py
+++ hello1/mysite/admin.py
@@ -3,6 +3,23 @@ from .models import Question,Choice
 
 # Register your models here.
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
 
-admin.site.register(Question)
+class QestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text','pub_date',]
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Title', {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QestionAdmin)
+
+
+# admin.site.register(Question)
 admin.site.register(Choice)
